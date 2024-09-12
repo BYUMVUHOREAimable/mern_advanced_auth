8c4e9ef5d0346bc237217c1eced64641fe830f6b
BYUMVUHOREAimable <122086815+BYUMVUHOREAimable@users.noreply.github.com>
2024-09-12 11:42:51 +0200

    Update README.md

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,3 +1,7 @@
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
