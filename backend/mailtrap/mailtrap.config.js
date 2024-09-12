33897e825b036f7c83a0f06c9da9a662b6562fa2
BYUMVUHOREAimable <aimablebyumvuhore@gmail.com>
2024-09-12 09:42:48 +0200

    Project completed

---
+++ backend/mailtrap/mailtrap.config.js
@@ -0,0 +1,15 @@
+import { MailtrapClient } from "mailtrap";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const mailtrapClient = new MailtrapClient({
+	endpoint: process.env.MAILTRAP_ENDPOINT,
+	token: process.env.MAILTRAP_TOKEN,
+});
+
+export const sender = {
+	email: "mailtrap@demomailtrap.com",
+	name: "BYUMVUHORE Aimable",
+};
+
